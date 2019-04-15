7b61f90ea45ffcb3f762333c8d99f877a5089c0e
fywest <fywest2109@hotmail.com>
2019-04-15 23:54:43 +0300

    script for check service done

--- week2/check_service.sh
+++ week2/check_service.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-sudo service mysql status
+
+service_name=$1
+echo "<----------------$service_name---------------->" >> status.txt
+
+if [ ! -f "/etc/init.d/$service_name" ]; then
+    echo "Error:Service Not Found"
+    exit 1
+fi
+
+sudo service $service_name status &>> status.txt 
+retVal=$?
+if [ $retVal -eq 0  ]; then
+    echo "is Running"
+else
+    sudo service $service_name start &>> status.txt 
+    echo "Restarting"
+fi
