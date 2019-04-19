03b79cd7999e1a3024304d6c099cdac2511ba95b
fywest <fywest2109@hotmail.com>
2019-04-19 23:50:22 +0300

    add Awk files

---
+++ week2/lab25_test.sh
@@ -0,0 +1,7 @@
+awk 'BEGIN{printf "%4s %4s %4s %4s %4s %4s %4s %4s %4s\n","FILENAME","ARGC","FNR","FS","NF","NR","OFS","ORS","RS";printf "---------------------------------------------\n"} {printf "%4s %4s %4s %4s %4s %4s %4s %4s %4s\n",FILENAME,ARGC,FNR,FS,NF,NR,OFS,ORS,RS}'  lab25_test.txt
+
+awk -F\' 'BEGIN{printf "%4s %4s %4s %4s %4s %4s %4s %4s %4s\n","FILENAME","ARGC","FNR","FS","NF","NR","OFS","ORS","RS";printf "---------------------------------------------\n"} {printf "%4s %4s %4s %4s %4s %4s %4s %4s %4s\n",FILENAME,ARGC,FNR,FS,NF,NR,OFS,ORS,RS}'  lab25_test.txt
+
+awk '{print NR,FNR,$1,$2,$3}' lab25_test.txt
+
+awk '{print $1,$2,$5}' OFS=" $ "  lab25_test.txt
